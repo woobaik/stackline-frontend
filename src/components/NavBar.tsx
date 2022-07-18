76d126cd7b664f6280039ed6755547823dbbfb1c
Joungwoo Baik <joungwoo.baik@gmail.com>
2022-07-18 20:08:01 +0900

    Fix bug :: navbar stretch to the end when the page is on the mobile view

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import StackLineLogo from '../assets/images/stackline_logo.svg';
 
 const NavBar = () => {
   return (
-    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100vw' }}>
       <div className="logo">
         <img src={StackLineLogo} alt="stackline logo" />
       </div>
